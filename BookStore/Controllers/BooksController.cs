014cb328844f63d34b0135b0e64024cd49ab9819
Robooto <samrussell63@gmail.com>
2015-08-23 23:31:26 -0700

    Final

--- BookStore/Controllers/BooksController.cs
+++ BookStore/Controllers/BooksController.cs
@@ -9,9 +9,13 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Description;
 
 namespace BookStore.Controllers
 {
+    /// <summary>
+    /// Use this API to get all the books, search for a specific criteria or get a single book
+    /// </summary>
     public class BooksController : ApiController
     {
         private IUnitOfWork unit;
@@ -21,7 +25,12 @@ namespace BookStore.Controllers
             this.unit = unit;
         }
 
+        /// <summary>
+        /// Retrieves all books registered on the system
+        /// </summary>
+        /// <returns>Collection of book entities</returns>
         [Queryable]
+        [ResponseType(typeof(IEnumerable<BookDto>))]
         public IHttpActionResult Get()
         {
             var books = unit.Books.All;
@@ -31,7 +40,13 @@ namespace BookStore.Controllers
             return Ok(response);
         }
 
+        /// <summary>
+        ///  Searches books for a specific query
+        /// </summary>
+        /// <param name="query">Query text to search</param>
+        /// <returns>Filtered list based on search criteria</returns>
         [Queryable]
+        [ResponseType(typeof(IEnumerable<BookDto>))]
         public IHttpActionResult Get(string query)
         {
             var results = unit.Books.Search(query);
@@ -46,7 +61,13 @@ namespace BookStore.Controllers
             return Ok(response);
         }
 
+        /// <summary>
+        /// Gets a single book
+        /// </summary>
+        /// <param name="id">Unique identifier for book</param>
+        /// <returns>Single book entity</returns>
         [CheckNulls]
+        [ResponseType(typeof(BookDto))]
         public IHttpActionResult Get(int id)
         {
             var result = unit.Books.GetOne(id);
